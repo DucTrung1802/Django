1376dcca74db4cf6675c365cbd55e971b92dc288
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-03 03:02:39 +0700

    #13 vid 03-001 at 0:00

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,20 +3,20 @@ from django.http import HttpResponse
 
 projectList = [
     {
-        "userId": 1,
-        "id": 1,
+        "userId": "101",
+        "id": "1",
         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
     },
     {
-        "userId": 1,
-        "id": 2,
+        "userId": "202",
+        "id": "2",
         "title": "qui est esse",
         "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
     },
     {
-        "userId": 1,
-        "id": 3,
+        "userId": "303",
+        "id": "3",
         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
     },
@@ -31,4 +31,8 @@ def projects(request):
 
 
 def project(request, pk):
-    return render(request, "projects/single_project.html")
+    projectObj = None
+    for i in projectList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, "projects/single_project.html", {"project": projectObj})
