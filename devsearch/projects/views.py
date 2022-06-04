99938ab80f79c539bb356e3b13c8274519f7cc77
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-04 16:52:21 +0700

    #18 04-001 at 0:00

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 projectList = [
     {
@@ -24,15 +25,15 @@ projectList = [
 
 
 def projects(request):
-    page = "projects"
-    number = 10
-    context = {"page": page, "number": number, "projects": projectList}
+    projects = Project.objects.all()
+    context = {"projects": projects}
     return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectList:
-        if i["id"] == pk:
-            projectObj = i
-    return render(request, "projects/single_project.html", {"project": projectObj})
+    projectObj = Project.objects.get(id=pk)
+    return render(
+        request,
+        "projects/single_project.html",
+        {"projectObj": projectObj},
+    )
